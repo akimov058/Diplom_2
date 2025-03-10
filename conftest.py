d90173b3db25c98eff241489b4548e4170349f02
Akimov058 <YuAkimov.beeline@yandex.ru>
2025-03-10 22:55:53 +0300

    создал тест кейс test_create_user и мелкие правки

--- conftest.py
+++ conftest.py
@@ -4,15 +4,18 @@ from faker import Faker
 
 @pytest.fixture()
 def generate_random_email():
-    email = Faker.email(locale="ru_RU")
+    fake = Faker()
+    email = fake.email()
     return email
 
 @pytest.fixture()
 def generate_random_password():
-    password = Faker.password(length=10)
+    fake = Faker()
+    password = fake.password(length=10)
     return password
 
 @pytest.fixture()
 def generate_random_name():
-    name = Faker.name(locale="ru_RU")
+    fake = Faker()
+    name = fake.name()
     return name
--- tests/test_create_user.py
+++ tests/test_create_user.py
@@ -1,6 +1,7 @@
 import allure
 import requests
 import pytest
+from methods.create_user import CreateUser
 
 class TestCreateUser:
     @allure.title('Создание уникального пользователя')
@@ -8,4 +9,9 @@ class TestCreateUser:
         email = generate_random_email
         password = generate_random_password
         name = generate_random_name
+        response = CreateUser.post_create_login(email,password,name)
+        with allure.step('Проверяем код и текст ответа'):
+            assert response.status_code == 200 and 'success' in response.text
+
+
 
