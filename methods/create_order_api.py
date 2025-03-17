2c11dbd00136b6f7bab59180d27e9eee03848a96
Akimov058 <YuAkimov.beeline@yandex.ru>
2025-03-17 22:21:17 +0300

    Написал тесты  test_create_order_with_authorization_and_no_ingridients

--- methods/create_order_api.py
+++ methods/create_order_api.py
@@ -5,10 +5,14 @@ from urls.urls import Urls
 class CreateOrderApi:
     @staticmethod
     @allure.step('Вызов метода создания заказа')
-    def post_create_order(token):
-        payload = {
-            "ingredients": ["61c0c5a71d1f82001bdaaa70"]
-        }
+    def post_create_order(token=None,ingredients=False):
+        if ingredients == True:
+            payload = {
+                "ingredients": ["61c0c5a71d1f82001bdaaa70"]
+            }
+        else:
+            payload = {
+            }
         header = {"Authorization": token}
         response = requests.post(Urls.URL_CREATE_ORDER,json=payload,headers=header)
         return response
--- tests/test_create_order.py
+++ tests/test_create_order.py
@@ -10,11 +10,31 @@ class TestCreateOrder:
         name = generate_random_name
         response_create_user = BaseApi.post_create_login(email,password,name)
         token = response_create_user.json()['accessToken']
-        response_create_order = CreateOrderApi.post_create_order(token)
+        response_create_order = CreateOrderApi.post_create_order(token,True)
         assert response_create_order.status_code == 200 and response_create_order.json()['success']==True
 
     @allure.title('Создание заказа без авторизации')
     def test_create_order_not_authorization(self):
-        response_create_order = CreateOrderApi.post_create_order('')
+        response_create_order = CreateOrderApi.post_create_order(ingredients=True)
         assert response_create_order.status_code == 200 and response_create_order.json()['success']==True
+
+    @allure.title('Создание заказа с ингридиентами')
+    def test_create_order_with_authorization_and_ingridients(self,generate_random_email,generate_random_password,generate_random_name):
+        email = generate_random_email
+        password = generate_random_password
+        name = generate_random_name
+        response_create_user = BaseApi.post_create_login(email,password,name)
+        token = response_create_user.json()['accessToken']
+        response_create_order = CreateOrderApi.post_create_order(token,True)
+        assert response_create_order.status_code == 200 and response_create_order.json()['success']==True
+
+    @allure.title('Создание заказа без ингридиентов')
+    def test_create_order_with_authorization_and_no_ingridients(self,generate_random_email,generate_random_password,generate_random_name):
+        email = generate_random_email
+        password = generate_random_password
+        name = generate_random_name
+        response_create_user = BaseApi.post_create_login(email,password,name)
+        token = response_create_user.json()['accessToken']
+        response_create_order = CreateOrderApi.post_create_order(token,False)
+        assert response_create_order.status_code == 400 and response_create_order.json()['success']==False
 
