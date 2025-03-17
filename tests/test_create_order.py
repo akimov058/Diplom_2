42c94686e091e1eb4213bdbd6d0b4f570a2f37c0
Akimov058 <YuAkimov.beeline@yandex.ru>
2025-03-17 22:11:31 +0300

    Написал тесты  test_create_order_not_authorization

--- tests/test_create_order.py
+++ tests/test_create_order.py
@@ -4,7 +4,7 @@ from methods.create_order_api import CreateOrderApi
 
 class TestCreateOrder:
     @allure.title('Создание заказа с авторизацией')
-    def test_create_order(self,generate_random_email,generate_random_password,generate_random_name):
+    def test_create_order_with_authorization(self,generate_random_email,generate_random_password,generate_random_name):
         email = generate_random_email
         password = generate_random_password
         name = generate_random_name
@@ -12,3 +12,9 @@ class TestCreateOrder:
         token = response_create_user.json()['accessToken']
         response_create_order = CreateOrderApi.post_create_order(token)
         assert response_create_order.status_code == 200 and response_create_order.json()['success']==True
+
+    @allure.title('Создание заказа без авторизации')
+    def test_create_order_not_authorization(self):
+        response_create_order = CreateOrderApi.post_create_order('')
+        assert response_create_order.status_code == 200 and response_create_order.json()['success']==True
+
